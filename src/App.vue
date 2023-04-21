a53fca33580acac7c0eeb43723016cd9a5b738aa
Arturo <arturotinajas@gmail.com>
2023-04-21 17:46:25 +0200

    List

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,13 @@ const addTodo = () => {
     done: false,
     createdAt: new Date().getTime()
   })
+
+  input_content.value = ''
+  input_category.value = null
+}
+
+const removeTodo = () => {
+  todos.value = todos.value.filter(td => td!==todo )
 }
 
 watch(todos, newVal => {
@@ -36,6 +43,7 @@ watch(name, (newVal) => {
 
 onMounted(() => {
   name.value = localStorage.getItem('name') || ''
+  todos.value = JSON.parse(localStorage.getItem('todos')) || []
 })
 </script>
 
@@ -87,6 +95,27 @@ onMounted(() => {
         <input type="submit" value="Add todo" />
       </form>
     </section>
+
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list">
+        <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+          <label>
+            <input type="checkbox" v-modal="todos.done">
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+
+          <div class="todo-content">
+            <input type="text" v-model="todo.content" />
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+        </div>
+      </div>
+    </section>
+
   </main>
 
 </template>
